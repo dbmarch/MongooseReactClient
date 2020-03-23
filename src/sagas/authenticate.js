a61a6af940fa469c08908b0bb6cf275b238f4f26
Dave March <dbmarch@gmail.com>
2020-03-23 08:04:37 -0400

    cleanup

--- src/sagas/authenticate.js
+++ src/sagas/authenticate.js
@@ -2,21 +2,12 @@ import { takeLatest, select, put } from 'redux-saga/effects'
 
 import {
 	LOGIN_USER,
-	// REGISTER_USER,
-	// CONFIRM_USER,
-	// RESEND_CONFIRMATION_CODE,
 	GET_SIGNED_IN_USER,
 	LOGOUT_USER,
 } from '../actions/action-types'
 
 import { getUser } from '../selectors'
 import { setAuthorized } from '../actions'
-// import { idTokenSelector } from '../selectors/authSelectors'
-// import Promise from 'bluebird'
-// import get from 'lodash/get'
-
-// const loginAsync = user => {
-// }
 
 
 export function* doLoginUser() {
@@ -55,29 +46,13 @@ export function* doGetSignedInUser() {
 		return
 	}
 
-	// try {
-	// 	const session = yield call(obtainSessionAsync)
-	// 	console.info('Session', session)
-	// 	yield put(setSession(session))
-	// 	yield put(setAuthorized(session.isValid()))
-	// 	console.info('currentUser = ', userPool.getCurrentUser())
-	// } catch (err) {
-	// 	console.error('Get signed in user failed ', err)
-	// 	yield put(setAuthError({ error: err.code, description: err.message }))
-	// }
 }
 
 function* doLogout() {
 	console.info('doLogout')
 
-	// try {
-	// 	userPool.getCurrentUser().signOut()
-	// } catch (err) {
-	// 	console.info(err)
-	// }
 	try {
 		yield put(setAuthorized(false))
-		// yield put(setSession({}))
 	} catch (err) {
 		console.info(err)
 	}
